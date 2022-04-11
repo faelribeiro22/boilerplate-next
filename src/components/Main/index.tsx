f1dfd0207fc03a2a505de8aabb90b2cff8a73636
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2022-04-10 23:46:43 -0300

    adicionando um texto

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -7,7 +7,7 @@ const Main = ({
   <S.Wrapper>
     <S.Logo
       src="/img/logo.svg"
-      alt="Imagem de um átomo e React Avançado escrito ao lado."
+      alt="Imagem de um átomo e React Avançado escrito ao lado. é só um teste."
     />
     <S.Title>{title}</S.Title>
     <S.Description>{description}</S.Description>
