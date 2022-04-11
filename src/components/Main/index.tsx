0896026f0fccca3c62bc02ddff2883809f73a552
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2022-04-10 23:56:43 -0300

    remove teste (#413)

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -7,7 +7,7 @@ const Main = ({
   <S.Wrapper>
     <S.Logo
       src="/img/logo.svg"
-      alt="Imagem de um átomo e React Avançado escrito ao lado. é só um teste."
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
     />
     <S.Title>{title}</S.Title>
     <S.Description>{description}</S.Description>
